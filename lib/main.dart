dcd36dcfd8ae98ad75d90d6d18e571d42cfa491d
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-06-07 21:27:58 -0500

    changed colors and added appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Card(),
+  ));
+}
+
+class Card extends StatelessWidget {
+  const Card({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[500],
+      appBar: AppBar(
+        title: const Text("ID card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+        elevation: 0.0,
+      ),
+      body: const Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 10.0),
+      ),
+    );
+  }
 }
